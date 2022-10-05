40d622c438e49e4e61387d7ae7ec0c571082f8f8
ermakovaDev <ermakova.dev@yandex.ru>
2022-10-05 10:01:52 +0300

    add recyclerView adapter

--- app/src/main/java/me/chronick/weatherapp/adapters/WeatherAdapter.kt
+++ app/src/main/java/me/chronick/weatherapp/adapters/WeatherAdapter.kt
@@ -1,14 +1,17 @@
 package me.chronick.weatherapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import me.chronick.weatherapp.R
 import me.chronick.weatherapp.databinding.ListItemBinding
 
-class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
 
-    class Holder(view: View) : RecyclerView.ViewHolder(view){
+    class Holder(view: View) : RecyclerView.ViewHolder(view){ // creating pattern markup
         private val binding = ListItemBinding.bind(view)
         fun bind(item: WeatherModel) = with(binding){
             tvDateTime.text = item.dataTime
@@ -17,11 +20,22 @@ class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        TODO("Not yet implemented")
+    class Comparator: DiffUtil.ItemCallback<WeatherModel>(){
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
     }
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder { // the number of views is created by the number of items in the list
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false) // parent:ViewGroup contains context
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) { // how to fill Holder (stores all references to positions) in and position
+        holder.bind(getItem(position))
     }
 }
--- app/src/main/java/me/chronick/weatherapp/fragments/HoursFragment.kt
+++ app/src/main/java/me/chronick/weatherapp/fragments/HoursFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import me.chronick.weatherapp.R
+import me.chronick.weatherapp.adapters.WeatherAdapter
+import me.chronick.weatherapp.adapters.WeatherModel
 import me.chronick.weatherapp.databinding.FragmentHoursBinding
 import me.chronick.weatherapp.databinding.FragmentMainBinding
 
 class HoursFragment : Fragment() {
 
     private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,24 @@ class HoursFragment : Fragment() {
     ): View {
         binding = FragmentHoursBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcViewItem()
+    }
+
+    private fun initRcViewItem() = with(binding){
+        rcViewItem.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcViewItem.adapter = adapter
+
+        val listTest = listOf(
+            WeatherModel("", "12:00", "Sunny", "", "32C", "", "", "", ""),
+                    WeatherModel("", "15:00", "Rain", "", "20C", "", "", "", ""),
+        WeatherModel("", "10:00", "Snow", "", "21C", "", "", "", "")
+        )
+        adapter.submitList(listTest)
     }
 
     companion object {
